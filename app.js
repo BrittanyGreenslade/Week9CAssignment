2ca4c8b75468a11e1b9c1e240968944d758010da
Brittany <bgreensl@ualberta.ca>
2021-03-21 17:23:35 -0600

    completed WK9E Assignment

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-//Collaborated with Shawn
-//If RJ asks for help tomorrow with the bonus of this one I will try to explain because it helps me understand better but as of me handing this in now he hasn't asked yet
+//Collaborated with Shawn for wk9c
+//didn't collaborate for wk9d or wk9E
 
+//wk9c Stuff
 //This was the first part before the bonus
 // This one was easy after the last assignment.
 // created an array with the different tweets as strings for items,
@@ -23,8 +24,8 @@
 //   console.log(tweet[counter]);
 // }
 
-// created this array with the objects (which are inside of {}) according to your PPTs
-//keys on left of : and values on right of : within {}
+// (WK9C) created this array with the objects (which are inside of {}) according to your PPTs
+//(WK9C)keys on left of : and values on right of : within {}
 var completeTweets = [
   {
     tweet: `Yea your booty is nice but how thicc is your love for the environment.`,
@@ -54,7 +55,7 @@ var completeTweets = [
     tweet: `Is there an Uber eats that gets you the food but also feeds it to you and asks you about your day?`,
     username: `@Adamripp`,
     createdAt: `03-18-2018`,
-    age: 25,
+    age: 16,
   },
   {
     tweet: `@gerardway when you step on a scale does it tell you how much Gerard weighs please respond`,
@@ -72,7 +73,7 @@ var completeTweets = [
     tweet: `U a software update? because not now`,
     username: `@pakalupapito`,
     createdAt: `06-23-2017`,
-    age: 55,
+    age: 14,
   },
   {
     tweet: `2020 is what 2012 tried so hard to be`,
@@ -84,24 +85,47 @@ var completeTweets = [
     tweet: `My closet is like 16 shirts I plan to fit into again and 1 shirt I wear every day`,
     username: `@TheCatWhisperer`,
     createdAt: `03-15-2019`,
-    age: 27,
+    age: 12,
   },
 ];
+// console.log(completeTweets);
 
+// WK9E stuff -  declaration of the function
+//'the function legalAge takes the argument ageOfUSers and applies the conditional statements within it to
+//the data in the variable above; it just returns 'true' when ran this way though because the age of the first
+//user is >=18
+
+//I'm a bit confused how it knows to reference the data set 'complete tweets' though??? obviously it's the only
+//one I have on the page here but what if it wasn't? is it because of '.age'?
+function legalAge(agesOfUsers) {
+  if (agesOfUsers.age >= 18) {
+    return true;
+  } else if (agesOfUsers.age < 18) {
+    return false;
+  } else {
+    console.log(`Please try again`);
+  }
+}
+
+//filtering out any objects that the function 'legalAge' found to have 'age' keys with values over 18
+var eighteenPlus = completeTweets.filter(legalAge);
+console.log(eighteenPlus);
+
+// Wk9C Stuff
 //This calculates the total number of objects('items') inside of the array
-var tweetsArray = completeTweets.length;
+// var tweetsArray = completeTweets.length;
 
 //ran this 'for' loop again with the console logs for each of the keys within the different completeTweets objects
-for (var i = 0; i < tweetsArray; i++) {
-  // console.log(completeTweets[i].tweet);
-  // console.log(completeTweets[i].username);
-  // console.log(completeTweets[i].createdAt);
-  // console.log(completeTweets[i].age);
-  console.log(
-    `Tweet:${completeTweets[i].tweet} \nUsername: ${completeTweets[i].username} \nCreated At:${completeTweets[i].createdAt} \nAge:${completeTweets[i].age}`
-  );
-}
-//Here, Shawn suggested I try creating these template literals with variables in them to make my code more DRY which was actually not that tough
+// for (var i = 0; i < tweetsArray; i++) {
+//   // console.log(completeTweets[i].tweet);
+//   // console.log(completeTweets[i].username);
+//   // console.log(completeTweets[i].createdAt);
+//   // console.log(completeTweets[i].age);
+//   console.log(
+//     `Tweet:${completeTweets[i].tweet} \nUsername: ${completeTweets[i].username} \nCreated At:${completeTweets[i].createdAt} \nAge:${completeTweets[i].age}`
+//   );
+// }
+//Here, Shawn suggested I try creating these template literals with variables in them to make my code more DRY which v easy
 //Also since you can put text and variables in these I labelled each new line
 //I made each of the variables equal to what I had  individually console-logged above, because I'm calling them again but all together within my template literal
 // A combo of you saying \n created a new line and also google, because I hated how it looked when it was all blobbed together :)
